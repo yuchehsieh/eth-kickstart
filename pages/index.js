d5e826f60368f9ee1e55c3d61df6938bbfdd1270
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-04-29 17:52:22 +0800

    usgin Next.js lifecycle method - getInitialProps() to fetch the data then inject as component's props

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return {campaigns};
+    }
 
     async componentDidMount() {
-        const addresses = await factory.methods.getDeployedCampaigns().call();
-        console.log(addresses);
+        // const addresses = await factory.methods.getDeployedCampaigns().call();
+        // console.log(addresses);
     }
 
     render() {
         return (
-            <h1>Show a list of Campaigns</h1>
+            <div>
+                <h1>Show a list of Campaigns</h1>
+                <p>{this.props.campaigns[0]}</p>
+            </div>
         )
     }
 }
