aab5f98ee8d2f7aa2f5f98e5a429e44b29b9c98a
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-04-29 15:43:09 +0800

    create campaign factory instance by fetching the exist contract

---
+++ ethereum/factory.js
@@ -0,0 +1,20 @@
+import web3 from "./web3";
+import CampaignFactory from './build/CampaignFactory';
+
+let factory;
+
+const getFactory = async () => {
+    factory = await new web3.eth.Contract(JSON.parse(CampaignFactory.interface), '0x257fCACb9df2fE1c6bda0D505862aD4371Bd5816');
+};
+
+getFactory().catch();
+
+/** Teacher's solution! **/
+// const instance = new web3.eth.Contract(
+//     JSON.parse(CampaignFactory.interface),
+//     '0x257fCACb9df2fE1c6bda0D505862aD4371Bd5816'
+// );
+//
+// export default instance;
+
+export default factory;
