ff8fd23d30da28c6bbe90991f2e3c2effb8a6013
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-05-01 09:33:40 +0800

    include the style tag by adding <link> directly in the render method

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import factory from '../ethereum/factory';
-import 'semantic-ui-css/semantic.min.css'
+
+// import 'semantic-ui-css/semantic.min.css';
+import {Card} from 'semantic-ui-react'
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -8,17 +10,33 @@ class CampaignIndex extends Component {
         return {campaigns};
     }
 
-    async componentDidMount() {
-        // const addresses = await factory.methods.getDeployedCampaigns().call();
-        // console.log(addresses);
+    renderCampaigns() {
+        // {
+        //     header: 'Project Report - April',
+        //     description: 'Leverage agile frameworks to provide a robust synopsis for high level overviews.',
+        //     meta: 'ROI: 30%',
+        // },
+        const items = this.props.campaigns.map(campaign => {
+            return {
+                header: campaign,
+                description: <a>View Campaign</a>,
+                fluid: true,
+                // meta: 'BBB'
+            }
+        });
+        return <Card.Group items={items}/>
     }
 
     render() {
         return (
-            <div>
-                <h1>Show a list of Campaigns</h1>
-                <p>{this.props.campaigns[0]}</p>
-            </div>
+            <>
+                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+                <div>
+                    <h1>Show a list of Campaigns</h1>
+                    <p>{this.props.campaigns[0]}</p>
+                    {this.renderCampaigns()}
+                </div>
+            </>
         )
     }
 }
