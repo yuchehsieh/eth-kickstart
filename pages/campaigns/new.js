438cfe62f6fe14cd75522bce3f5be9fce887ccd1
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-05-01 18:15:12 +0800

    add loading spinner and form validation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,39 +1,50 @@
 import React, {Component} from 'react';
 import Layout from "../../components/Layout";
-import {Form, Button, Input} from 'semantic-ui-react';
+import {Form, Button, Input, Message} from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
 
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: '',
+        loading: false,
     };
 
     async onSubmit(e) {
         e.preventDefault();
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods.createCampaign(this.state.minimumContribution).send({
-            from: accounts[0]
-            /**
-             *  when running in the browser,
-             *  Metamask automatically calculate the gas for us
-             *  don't have to specify gas amount!
-             * **/
-        })
+        this.setState({loading: true});
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods.createCampaign(this.state.minimumContribution).send({
+                from: accounts[0]
+                /**
+                 *  when running in the browser,
+                 *  Metamask automatically calculate the gas for us
+                 *  don't have to specify gas amount!
+                 * **/
+            });
+        } catch (e) {
+            this.setState({
+                errorMessage: e.message
+            });
+        }
+        this.setState({loading: false});
     }
 
     render() {
         return (
             <Layout>
                 <h3>New Campaign!</h3>
-                <Form onSubmit={this.onSubmit.bind(this)}>
+                <Form onSubmit={this.onSubmit.bind(this)} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input label="wei" labelPosition="right" value={this.state.minimumContribution}
                                onChange={e => this.setState({minimumContribution: e.target.value})}/>
                     </Form.Field>
-                    <Button primary type='submit'>Submit</Button>
+                    <Message error header="Oops!" content={this.state.errorMessage}/>
+                    <Button primary type='submit' loading={this.state.loading}>Submit</Button>
                 </Form>
             </Layout>
         );
