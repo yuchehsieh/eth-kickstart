ee19cf70e99a43b0a7796447d58ea1d64f9f8377
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-05-01 11:09:26 +0800

    add basic Form by semantic-ui setup

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
 import Layout from "../../components/Layout";
-
+import {Form, Button, Input} from 'semantic-ui-react';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
+
+    state = {
+        minimumContribution: ''
+    };
+
+    async onSubmit(e) {
+        e.preventDefault();
+        const accounts = await web3.eth.getAccounts();
+        await factory.methods.createCampaign(this.state.minimumContribution).send({
+            from: accounts[0]
+            /**
+             *  when running in the browser,
+             *  Metamask automatically calculate the gas for us
+             *  don't have to specify gas amount!
+             * **/
+        })
+    }
+
     render() {
         return (
             <Layout>
-                <h1>New Campaign!</h1>
+                <h3>New Campaign!</h3>
+                <Form onSubmit={this.onSubmit.bind(this)}>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <Input label="wei" labelPosition="right" value={this.state.minimumContribution}
+                               onChange={e => this.setState({minimumContribution: e.target.value})}/>
+                    </Form.Field>
+                    <Button primary type='submit'>Submit</Button>
+                </Form>
             </Layout>
         );
     }
