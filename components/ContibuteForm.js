6c093c66c0bc652ed0683cb28da5d122996c694f
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-05-03 13:23:41 +0800

    using same Campaign utility to fulfill contribute method

---
+++ components/ContibuteForm.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {Form, Button, Input, Message} from 'semantic-ui-react';
+import Campaign from '../ethereum/campaign';
+import web3 from "../ethereum/web3";
+import { Router } from '../routes';
+
+class ContibuteForm extends Component {
+
+    state = {
+        value: '',
+        errorMessage: '',
+        loading: false,
+    };
+
+    async onSubmit(e) {
+        e.preventDefault();
+        this.setState({loading: true});
+        const campaign = Campaign(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether'),
+                data: web3.utils.stringToHex('TEST string input')
+                /**
+                 *  when running in the browser,
+                 *  Metamask automatically calculate the gas for us
+                 *  don't have to specify gas amount!
+                 * **/
+            });
+            Router.replaceRoute(`/campaigns/${this.props.address}`)
+            // Router.pushRoute('/');
+        } catch (e) {
+            this.setState({
+                errorMessage: e.message
+            });
+        }
+        this.setState({loading: false});
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.onSubmit.bind(this)} error={!!this.state.errorMessage}>
+                <Form.Field>
+                    <label>Amount to Contribute</label>
+                    <Input label="ether" labelPosition="right" value={this.state.value}
+                           onChange={e => this.setState({value: e.target.value})}/>
+                </Form.Field>
+                <Message error header="Oops!" content={this.state.errorMessage}/>
+                <Button primary type='submit' loading={this.state.loading}>Contribute</Button>
+            </Form>
+        );
+    }
+}
+
+export default ContibuteForm;
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
-import {Card, Button} from 'semantic-ui-react'
+import {Card, Button, Grid} from 'semantic-ui-react'
 import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContibuteForm';
 
 
 class CampaignShow extends Component {
-    static async getInitialProps (props) {
+    static async getInitialProps(props) {
         const campaign = await Campaign(props.query.address);
 
         const summary = await campaign.methods.getSummary().call();
 
         return {
+            address: props.query.address,
             minimumContribution: summary[0],
             balance: summary[1],
             requestsCount: summary[2],
@@ -60,15 +62,23 @@ class CampaignShow extends Component {
             }
         ];
 
-        return <Card.Group items={items} />
+        return <Card.Group items={items}/>
     }
 
     render() {
-        // console.log(this.props);
+        console.log(web3.utils.stringToHex('TEXT'));
+        console.log(web3.utils.hexToUtf8(web3.utils.stringToHex('TEXT')));
         return (
             <Layout>
                 <h3>Campaign show!</h3>
-                {this.renderCards()}
+                <Grid>
+                    <Grid.Column width={10}>
+                        {this.renderCards()}
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                        <ContributeForm address={this.props.address}/>
+                    </Grid.Column>
+                </Grid>
             </Layout>
         );
     }
