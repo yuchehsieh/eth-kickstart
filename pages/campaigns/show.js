4ed2ab4147ac7e7358970df05b8b122be475a6df
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-05-03 09:35:14 +0800

    get the dynamic url property and used it get the campain contract

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
 import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
 
 class CampaignShow extends Component {
-    // static async getInitialProps ({query}) {
-    //     // query.slug
-    //     console.log(query.slug);
-    // }
+    static async getInitialProps (props) {
+        const campaign = await Campaign(props.query.address);
+
+        const summary = await campaign.methods.getSummary().call();
+
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        }
+    }
 
     render() {
-        console.log(this.props);
-
+        // console.log(this.props);
         return (
             <Layout>
                 <h3>Campaign show!</h3>
